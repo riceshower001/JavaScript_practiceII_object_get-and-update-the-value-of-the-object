eedd6faa33d258ba435b48eab1b5ca2260e3150f
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-03-01 21:30:05 +0900

    Add files via upload

---
+++ JavaScript_practiceII_object_get and update the value of the object.js
@@ -0,0 +1,12 @@
+// Let's get the value of an object.
+
+const character = {name: "◆◆◆ ◆◆◆", age: 14};
+
+// Please print the value corresponding to "name" of the object assigned to the constant "character".
+console.log(character.name);
+
+// Please update the "age" value of "character" to "20".
+character.age = 20;
+
+// Please print the value of "character".
+console.log(character);
